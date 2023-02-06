55bb2c714e9b0aa1bf971330c3dcab2dd55e4f50
Luis Castro <luish.castroc@gmail.com>
2023-02-06 11:58:42 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-ngxs-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'cypress-ngxs';
+}
